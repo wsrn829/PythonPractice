be88320d2aa336d06da9b93d4efcb070fb1969a9
Jessie Mason <jmason.p@gmail.com>
2023-03-07 17:55:19 -0800

    Add empty question set.

---
+++ favorites.py
@@ -0,0 +1,36 @@
+# Welcome to the second Git exercise.
+# We're in a Python file this time, but the rules are 
+# mostly the same.
+
+# Let's see what's different: 
+first_question = "What is your favorite color?"
+# In Python, values inside quotation marks like you see above
+# are called Strings. Strings can contain any combination of
+# letters or numbers, so they're the perfect place to write our
+# answers. Go ahead and write your answer to the first question  
+# in the string below:
+first_question_answer = "YOUR_ANSWER_HERE"
+
+# After you've committed and pushed, repeat the same process for 
+# the rest of the questions!
+
+second_question = "What is your favorite food?"
+second_question_answer = "YOUR_ANSWER_HERE"
+
+third_question = "Who is your favorite fictional character? ex. Mickey Mouse, Bugs Bunny"
+third_question_answer = "YOUR_ANSWER_HERE"
+
+"""
+!! Reminder that you should be: 
+  - adding your changes after you make them
+  - committing the changes you added
+  - pushing your commits to your forked repository
+  After answering each question!! 
+"""
+
+fourth_question = "What is your favorite animal?"
+fourth_question_answer = "YOUR_ANSWER_HERE"
+
+fifth_question = "What is your favorite programming language? (Hint: You can always say Python!!)"
+fifth_question_answer = "YOUR_ANSWER_HERE"
+
