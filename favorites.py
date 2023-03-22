ccb28b56d04aabe532cce9bbbd96107449f54d11
wsrn829 <wsrn829@gmail.com>
2023-03-21 18:55:32 -0700

    Answers to the first question

--- favorites.py
+++ favorites.py
@@ -1,17 +1,17 @@
 # Welcome to the second Git exercise.
-# We're in a Python file this time, but the rules are 
+# We're in a Python file this time, but the rules are
 # mostly the same.
 
-# Let's see what's different: 
+# Let's see what's different:
 first_question = "What is your favorite color?"
 # In Python, values inside quotation marks like you see above
 # are called Strings. Strings can contain any combination of
 # letters or numbers, so they're the perfect place to write our
-# answers. Go ahead and write your answer to the first question  
+# answers. Go ahead and write your answer to the first question
 # in the string below:
-first_question_answer = "YOUR_ANSWER_HERE"
+first_question_answer = "PURPLE"
 
-# After you've committed and pushed, repeat the same process for 
+# After you've committed and pushed, repeat the same process for
 # the rest of the questions!
 
 second_question = "What is your favorite food?"
@@ -21,11 +21,11 @@ third_question = "Who is your favorite fictional character? ex. Mickey Mouse, Bu
 third_question_answer = "YOUR_ANSWER_HERE"
 
 """
-!! Reminder that you should be: 
+!! Reminder that you should be:
   - adding your changes after you make them
   - committing the changes you added
   - pushing your commits to your forked repository
-  After answering each question!! 
+  After answering each question!!
 """
 
 fourth_question = "What is your favorite animal?"
@@ -33,4 +33,3 @@ fourth_question_answer = "YOUR_ANSWER_HERE"
 
 fifth_question = "What is your favorite programming language? (Hint: You can always say Python!!)"
 fifth_question_answer = "YOUR_ANSWER_HERE"
-
